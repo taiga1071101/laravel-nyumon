6a10dc94a4f51b83ca6a51fc9116ee24cf435f80
taiga1071101 <taigatukimi@gmail.com>
2025-02-27 17:19:19 +0900

    station6クリア

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiaryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/diary', function () {
-    return 'Hello!';
-});
+Route::get('/diary', [DiaryController::class, 'index'])->name('diary.index');
